c7944afc1bbb21a20eb9340d8478730072d7f47f
Wexort <wexort.com@gmail.com>
2019-05-28 20:45:54 +0200

    Done except solvable

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,23 +1,17 @@
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.StdOut;
-
-import java.util.Comparator;
+import edu.princeton.cs.algs4.Stack;
 
 public class Board {
     private int gridLen;
     private int[][] grid;
     private int[][] goal;
-    private int[][] ex;
     private int zeroi;
     private int zeroj;
     private int hamming;
     private int manhattan;
-    private int move;
 
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        move = 0;
         grid = blocks;
         gridLen = grid[0].length;
         goal = calcGoal();
@@ -26,33 +20,129 @@ public class Board {
         manhattan = calcManhattan();
     }
 
-    // Constructor to create neighbours
-    private Board(int[][] exGrid, int[][] newGrid, int mv) {
-        //StdOut.println("Use prive constructor to generate neighbours...");
-        move = mv;
-        ex = exGrid;
-        grid = newGrid;
-        gridLen = grid[0].length;
-        goal = calcGoal();
-        findZero();
-        hamming = calcHamming();
-        manhattan = calcManhattan();
-        //StdOut.println("ex grid of neighbour");
-        //printGrid(exGrid);
-        //StdOut.println("current grid of this neighbour");
-        //printGrid(newGrid);
+    public int dimension() {
+        return gridLen;
     }
 
-    private void printGrid(int[][] gridToPrint) {
+    public int hamming() {
+        return hamming;
+    }
+
+    public int manhattan() {
+        return manhattan;
+    }
+
+    public boolean isGoal() {
+        return equalsGrid(grid, goal);
+    }
+
+    public Board twin() {
+        int[][] twinGrid = new int[gridLen][gridLen];
+        for (int i = 0; i < gridLen; i++) {
+            twinGrid[i] = grid[i].clone();
+        }
+        int count = 0;
+        int aj = 0;
+        int bj = 0;
+
+        for (int j = 0; j < gridLen; j++) {
+            if (count == 2) break;
+            else if (count == 0) {
+                if (twinGrid[0][j] != 0) {
+                    aj = j;
+                    count++;
+                }
+            }
+            else if (count == 1) {
+                if (twinGrid[0][j] != 0) {
+                    bj = j;
+                    count++;
+                }
+            }
+        }
+
+        int tmp = twinGrid[0][aj];
+        twinGrid[0][aj] = twinGrid[0][bj];
+        twinGrid[0][bj] = tmp;
+
+        Board twin = new Board(twinGrid);
+        return twin;
+    }
+
+    public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+        if (this.gridLen != that.gridLen) return false;
+        return equalsGrid(this.grid, that.grid);
+
+    }
+
+    public Iterable<Board> neighbors() {
+        Stack<Board> nbs = new Stack<>();
+        // StdOut.printf("zeroi is %d, zeroj is %d \n", zeroi, zeroj);
+        if (zeroj - 1 >= 0) { // has left
+            // StdOut.println("Has left");
+            int[][] gLeft = swap(grid, zeroi, zeroj, zeroi, zeroj - 1);
+            Board bLeft = new Board(gLeft);
+            nbs.push(bLeft);
+        }
+        if (zeroj + 1 <= gridLen - 1) { // has right
+            // StdOut.println("Has right");
+            int[][] gRight = swap(grid, zeroi, zeroj, zeroi, zeroj + 1);
+            Board bRight = new Board(gRight);
+            nbs.push(bRight);
+        }
+
+        if (zeroi - 1 >= 0) { // has up
+            // StdOut.println("Has up");
+            int[][] gUp = swap(grid, zeroi, zeroj, zeroi - 1, zeroj);
+            Board bUp = new Board(gUp);
+            nbs.push(bUp);
+        }
+
+        if (zeroi + 1 <= gridLen - 1) { // has down
+            // StdOut.println("Has down");
+            int[][] gDown = swap(grid, zeroi, zeroj, zeroi + 1, zeroj);
+            Board bDown = new Board(gDown);
+            nbs.push(bDown);
+        }
+        return nbs;
+    }
+
+    public String toString() {
+
         StringBuilder s = new StringBuilder();
         s.append(gridLen + "\n");
         for (int i = 0; i < gridLen; i++) {
             for (int j = 0; j < gridLen; j++) {
-                s.append(String.format("%2d ", gridToPrint[i][j]));
+                s.append(String.format("%2d ", grid[i][j]));
             }
             s.append("\n");
         }
-        StdOut.printf(s.toString());
+        return s.toString();
+
+    }
+
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+        // StdOut.println("---Initial Board----");
+
+        // StdOut.println(initial);
+        // StdOut.println("----Neighbours----");
+        Iterable<Board> nbs = initial.neighbors();
+        for (Board nb : nbs) {
+            // StdOut.println(nb);
+            // StdOut.println("---nb---");
+        }
     }
 
     private void findZero() {
@@ -124,53 +214,6 @@ public class Board {
         return score;
     }
 
-
-    public int dimension() {
-        return gridLen;
-    }
-
-    public int hamming() {
-        return hamming;
-    }
-
-    public int manhattan() {
-        return manhattan;
-    }
-
-    public boolean isGoal() {
-        return equalsGrid(grid, goal);
-    }
-
-    public Board twin() {
-        int[][] twinGrid = grid.clone();
-        int count = 0;
-        int aj = 0;
-        int bj = 0;
-
-        for (int j = 0; j < gridLen; j++) {
-            if (count == 2) break;
-            else if (count == 0) {
-                if (twinGrid[0][j] != 0) {
-                    aj = j;
-                    count++;
-                }
-            }
-            else if (count == 1) {
-                if (twinGrid[0][j] != 1) {
-                    bj = j;
-                    count++;
-                }
-            }
-        }
-
-        int tmp = twinGrid[0][aj];
-        twinGrid[0][aj] = twinGrid[0][bj];
-        twinGrid[0][bj] = tmp;
-
-        Board twin = new Board(twinGrid);
-        return twin;
-    }
-
     private boolean equalsGrid(int[][] x, int[][] y) {
         for (int i = 0; i < gridLen; i++) {
             for (int j = 0; j < gridLen; j++) {
@@ -180,51 +223,9 @@ public class Board {
         return true;
     }
 
-    public boolean equals(Object y) {
-        if (y == this) return true;
-        if (y == null) return false;
-        if (y.getClass() != this.getClass()) return false;
-        Board that = (Board) y;
-        if (this.gridLen != that.gridLen) return false;
-        return equalsGrid(this.grid, that.grid);
-
-    }
-
-    public Iterable<Board> neighbors() {
-        MinPQ<Board> nbs = new MinPQ<Board>(new BoardComparator());
-        StdOut.printf("zeroi is %d, zeroj is %d \n", zeroi, zeroj);
-        if (zeroj - 1 >= 0) { // has left
-            StdOut.println("Has left");
-            int[][] gLeft = swap(grid, zeroi, zeroj, zeroi, zeroj - 1);
-            Board bLeft = new Board(grid, gLeft, move + 1);
-            nbs.insert(bLeft);
-        }
-        if (zeroj + 1 <= gridLen - 1) { // has right
-            StdOut.println("Has right");
-            int[][] gRight = swap(grid, zeroi, zeroj, zeroi, zeroj + 1);
-            Board bRight = new Board(grid, gRight, move + 1);
-            nbs.insert(bRight);
-        }
-
-        if (zeroi - 1 >= 0) { // has up
-            StdOut.println("Has up");
-            int[][] gUp = swap(grid, zeroi, zeroj, zeroi - 1, zeroj);
-            Board bUp = new Board(grid, gUp, move + 1);
-            nbs.insert(bUp);
-        }
-
-        if (zeroi + 1 <= gridLen - 1) { // has down
-            StdOut.println("Has down");
-            int[][] gDown = swap(grid, zeroi, zeroj, zeroi + 1, zeroj);
-            Board bDown = new Board(grid, gDown, move + 1);
-            nbs.insert(bDown);
-        }
-        return nbs;
-    }
-
     private int[][] swap(int[][] gridToSwap, int xi, int xj, int yi, int yj) {
-        //StdOut.printf("Now swap (%d, %d) with (%d, %d)\n", xi, xj, yi, yj);
-        //StdOut.printf("Before swap: x is %d, y is %d\n", gridToSwap[xi][xj], gridToSwap[yi][yj]);
+        // StdOut.printf("Now swap (%d, %d) with (%d, %d)\n", xi, xj, yi, yj);
+        // StdOut.printf("Before swap: x is %d, y is %d\n", gridToSwap[xi][xj], gridToSwap[yi][yj]);
         int[][] g = new int[gridLen][gridLen];
         for (int i = 0; i < gridLen; i++) {
             g[i] = gridToSwap[i].clone();
@@ -232,48 +233,7 @@ public class Board {
         int tmp = g[xi][xj];
         g[xi][xj] = g[yi][yj];
         g[yi][yj] = tmp;
-        //StdOut.printf("After swap: x is %d, y is %d\n", g[xi][xj], g[yi][yj]);
+        // StdOut.printf("After swap: x is %d, y is %d\n", g[xi][xj], g[yi][yj]);
         return g;
-    }
-
-    private class BoardComparator implements Comparator<Board> {
-        public int compare(Board b1, Board b2) {
-            return b1.manhattan() - b2.manhattan();
-        }
-    }
-
-
-    public String toString() {
-
-        StringBuilder s = new StringBuilder();
-        s.append(gridLen + "\n");
-        for (int i = 0; i < gridLen; i++) {
-            for (int j = 0; j < gridLen; j++) {
-                s.append(String.format("%2d ", grid[i][j]));
-            }
-            s.append("\n");
-        }
-        return s.toString();
-
-    }
-
-    public static void main(String[] args) {
-        // create initial board from file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        int[][] blocks = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);
-        StdOut.println("---Initial Board----");
-
-        StdOut.println(initial);
-        StdOut.println("----Neighbours----");
-        Iterable<Board> nbs = initial.neighbors();
-        for (Board nb : nbs) {
-            StdOut.println(nb);
-            StdOut.println("---nb---");
-        }
     }
 }
--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -5,27 +5,63 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
+    private MinPQ<SearchNode> pqMain;
+    private MinPQ<SearchNode> pqTwin;
+    private SearchNode nodeGoal;
+    private Stack<Board> solution;
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        solution = new Stack<>();
+        pqMain = new MinPQ<SearchNode>();
+        SearchNode nodeInit = new SearchNode(initial, 0, null);
+        pqMain.insert(nodeInit);
+
+        pqTwin = new MinPQ<SearchNode>();
+        SearchNode nodeInitTwin = new SearchNode(initial.twin(), 0, null);
+        pqTwin.insert(nodeInitTwin);
+
+        if (isSolvable()) {
+            int move = 0;
+            while (true) {
+                move++;
+                SearchNode nodeCurrent = pqMain.delMin();
+                if (nodeCurrent.board.isGoal()) {
+                    nodeGoal = nodeCurrent;
+                    createSolution();
+                    break;
+                }
+                Iterable<Board> nbs = nodeCurrent.board.neighbors();
+                for (Board n : nbs) {
+                    if (nodeCurrent.lastNode == null || !n.equals(nodeCurrent.lastNode.board)) {
+                        pqMain.insert(new SearchNode(n, move, nodeCurrent));
+                    }
+                }
+            }
+        }
 
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-
+        return true;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
+        if (!isSolvable()) return -1;
+        else return nodeGoal.mv;
 
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-
+        return solution;
     }
 
     // solve a slider puzzle (given below)
@@ -51,4 +87,36 @@ public class Solver {
                 StdOut.println(board);
         }
     }
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private int mv;
+        private int manhattan;
+        private Board board;
+        private SearchNode lastNode;
+
+        public SearchNode(Board b, int move, SearchNode predecessor) {
+            mv = move;
+            board = b;
+            lastNode = predecessor;
+            manhattan = b.manhattan();
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            return (manhattan + mv) - (that.manhattan + that.mv);
+        }
+    }
+
+    private void createSolution() {
+        SearchNode n = nodeGoal;
+        while (true) {
+            solution.push(n.board);
+            if (n.lastNode == null) {
+                break;
+            }
+            n = n.lastNode;
+        }
+    }
+
 }
+
